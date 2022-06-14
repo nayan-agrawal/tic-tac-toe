ae5cdaffa7ecc092c17ddb1f53f301479cbc7d9e
nayan-agrawal <nayanagrawal939@gmail.com>
2022-06-15 00:24:28 +0530

    TTT-3 Project Setup - Basic Tic Tac Toe Working Project

--- src/App.js
+++ src/App.js
@@ -23,22 +23,100 @@ const App = () => {
 
   const reloadGame = () => {
     // Reload Game
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
   }
 
   const checkIsWinner = () => {
     // To check for winner
+    if (itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty") {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (itemArray[3] === itemArray[4] &&
+      itemArray[3] === itemArray[5] &&
+      itemArray[3] !== "empty") {
+      setWinMessage(`${itemArray[3]} wins`);
+    } else if (itemArray[6] === itemArray[7] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[6] !== "empty") {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (itemArray[0] === itemArray[3] &&
+      itemArray[0] === itemArray[6] &&
+      itemArray[0] !== "empty") {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7] &&
+      itemArray[1] !== "empty") {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (itemArray[2] === itemArray[5] &&
+      itemArray[2] === itemArray[8] &&
+      itemArray[2] !== "empty") {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (itemArray[0] === itemArray[4] &&
+      itemArray[0] === itemArray[8] &&
+      itemArray[0] !== "empty") {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (itemArray[2] === itemArray[4] &&
+      itemArray[2] === itemArray[6] &&
+      itemArray[2] !== "empty") {
+      setWinMessage(`${itemArray[2]} wins`);
+    } 
+    if (!itemArray.includes("empty")) {
+      setWinMessage("Draw!!");
+    }
   }
 
   const changeItem = (itemNumber) => {
     // Verify if box is filled
+    if (winMessage) {
+      return toast(winMessage, {
+        type: "success"
+      })
+    }
+
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("Already Filled!", {
+        type: "error"
+      })
+    }
+
+    checkIsWinner();
   }
 
   return (
-    <div className="App">
-      <Icons name="cross" />
-      <Icons name="circle" />
-      <Icons name="" />
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="my-3">
+              <h1 className="text-primary text-uppercase text-center">{winMessage}</h1>
+              <Button color="success" block onClick={reloadGame}>
+                Reload Game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warning">
+              {isCross ? "Cross" : "Circle"} turns
+            </h1>
+          )}
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card onClick={() => changeItem(index)} color="warning">
+                <CardBody className="box">
+                  <Icons name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
